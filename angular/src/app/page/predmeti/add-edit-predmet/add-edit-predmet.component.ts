8347b57bb3d30c31acd4b5f53ed3f88fc39b01d5
Vladimir_Antin <antin502@gmail.com>
2018-05-10 20:02:19 +0200

    new model, -- angular fix

--- angular/src/app/page/predmeti/add-edit-predmet/add-edit-predmet.component.ts
+++ angular/src/app/page/predmeti/add-edit-predmet/add-edit-predmet.component.ts
@@ -1,6 +1,7 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA} from '@angular/material';
 import {TipAktivnostiService} from '../../../service/tip-aktivnosti.service';
+import {TipAktivnosti} from '../../../model/tip-aktivnosti';
 
 @Component({
   selector: 'app-add-edit-predmet',
@@ -10,13 +11,18 @@ import {TipAktivnostiService} from '../../../service/tip-aktivnosti.service';
 export class AddEditPredmetComponent implements OnInit {
 
   //separatorKeysCodes = [ENTER, 186]; // semicolon 186 code
-   tipovi_aktivnosti = [];
+   tipovi_aktivnosti: TipAktivnosti[] = []; selectedValues = [];
   constructor(@Inject(MAT_DIALOG_DATA) public data: any, private tipAkativnostiService: TipAktivnostiService) {
   }
 
   ngOnInit(): void {
-    this.tipAkativnostiService.getAll().subscribe(d => this.tipovi_aktivnosti = d);
+    this.tipAkativnostiService.getAll().subscribe(d => {
+      this.tipovi_aktivnosti = d;
+      const tips = this.data.predmet.aktivnosti.map(t => t.id);
+      this.selectedValues = this.tipovi_aktivnosti.filter(t => tips.findIndex(tip => tip===t.id)!==-1);
+    });
   }
   change() {
+    this.data.predmet.aktivnosti = this.selectedValues;
   }
 }
